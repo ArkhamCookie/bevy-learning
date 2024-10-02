241ac914b57169d655695b91e1630bc601a74122
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-10-02 15:50:02 -0500

    feat: handle spawning in sprites

--- pong/src/main.rs
+++ pong/src/main.rs
@@ -1,3 +1,58 @@
+use bevy::prelude::*;
+
+fn spawn_camera(mut commands: Commands) {
+	commands.spawn(Camera2dBundle::default());
+}
+
+fn spawn_players(mut commands: Commands) {
+	// Spawn Play Area
+	commands.spawn(SpriteBundle {
+		sprite: Sprite {
+			color: Color::BLACK,
+			custom_size: Some(Vec2::new(700.0, 500.0)),
+			..Default::default()
+		},
+		..Default::default()
+	});
+
+	// Spawn Player 1
+	commands.spawn((SpriteBundle {
+		transform: Transform::from_translation(Vec3::new(-300.0, 0.0, 0.0)),
+		sprite: Sprite {
+			color: Color::WHITE,
+			custom_size: Some(Vec2::new(10.0, 150.0)),
+			..Default::default()
+		},
+		..Default::default()
+	}, Paddle {
+		move_up: KeyCode::KeyW,
+		move_down: KeyCode::KeyS,
+	}));
+
+	// Spawn Player 2
+	commands.spawn((SpriteBundle {
+		transform: Transform::from_translation(Vec3::new(300.0, 0.0, 0.0)),
+		sprite: Sprite {
+			color: Color::WHITE,
+			custom_size: Some(Vec2::new(10.0, 150.0)),
+			..Default::default()
+		},
+		..Default::default()
+	}, Paddle {
+		move_up: KeyCode::ArrowUp,
+		move_down: KeyCode::ArrowDown,
+	}));
+}
+
+#[derive(Component)]
+struct Paddle {
+	move_up: KeyCode,
+	move_down: KeyCode,
+}
+
 fn main() {
-	println!("Hello, world!");
+	let mut app = App::new();
+	app.add_plugins(DefaultPlugins);
+	app.add_systems(Startup, (spawn_camera, spawn_players));
+	app.run();
 }
