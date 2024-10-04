ab0ff63421a6fe165e9f3dc87b375c0e96348801
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-10-04 00:05:00 -0500

    chore: run `rustfmt`

--- pong/src/components.rs
+++ pong/src/components.rs
@@ -1,10 +1,10 @@
 use bevy::prelude::*;
 
 #[derive(Component)]
-pub (crate) struct Paddle {
-	pub (crate) move_up: KeyCode,
-	pub (crate) move_down: KeyCode,
+pub(crate) struct Paddle {
+	pub(crate) move_up: KeyCode,
+	pub(crate) move_down: KeyCode,
 }
 
 #[derive(Component)]
-pub (crate) struct Ball;
+pub(crate) struct Ball;
